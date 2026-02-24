29d48ff951a2d2eb5e0836482f769777c00abd8a
Rangana Pradeep <123355131+PradeepGunawardhana@users.noreply.github.com>
2026-02-24 04:48:32 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,10 @@
+// create wen server
+const http = require('http');
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
+}   );
+server.listen(3000, 'localhost', () => {
+    console.log('Server running at http://localhost:3000/');
+});
